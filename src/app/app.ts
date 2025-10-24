d4584cb592614208a83a98f468d283e4c8bf89fc
DESKTOP-DLHK4Q0\Baqar9788 <muhbaqar9788@sbtjapan.com>
2025-10-24 18:31:09 +0500

    commit from vs code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [
+    RouterModule
+  ],
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.css'],
 })
 export class App {
-  protected readonly title = signal('v20');
+  title = 'homes';
 }
